ee7a5476b76171693cde2987536ba7f99dd5f3b7
Heis-mariyo <yoyoizunobi@gmail.com>
2026-04-20 22:26:26 +0100

    changed header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnDestroy, OnInit, inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +11,50 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
+export class Header implements OnInit, OnDestroy {
   isMobileMenuOpen = false;
+  isScrolled = false;
+  private readonly platformId = inject(PLATFORM_ID);
+  private readonly isBrowser = isPlatformBrowser(this.platformId);
+  private readonly router = inject(Router);
+  private navigationSubscription?: Subscription;
+
+  private readonly onScroll = () => {
+    this.isScrolled = window.scrollY > 14;
+  };
+
+  private readonly onResize = () => {
+    if (window.innerWidth >= 768) {
+      this.isMobileMenuOpen = false;
+    }
+  };
+
+  ngOnInit() {
+    if (!this.isBrowser) {
+      return;
+    }
+
+    this.onScroll();
+    this.navigationSubscription = this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.isMobileMenuOpen = false;
+        requestAnimationFrame(() => this.onScroll());
+      });
+
+    window.addEventListener('scroll', this.onScroll, { passive: true });
+    window.addEventListener('resize', this.onResize, { passive: true });
+  }
+
+  ngOnDestroy() {
+    if (!this.isBrowser) {
+      return;
+    }
+
+    window.removeEventListener('scroll', this.onScroll);
+    window.removeEventListener('resize', this.onResize);
+    this.navigationSubscription?.unsubscribe();
+  }
 
   toggleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
